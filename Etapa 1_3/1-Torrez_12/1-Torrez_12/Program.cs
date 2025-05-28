f46441a5496350f74da5de9aa0dea1f2d5cd670e
JzerroT <janinatorrezmendoza@gmail.com>
2025-05-28 19:40:17 -0300

    Add files via upload

---
+++ Etapa 1_3/1-Torrez_12/1-Torrez_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Torrez_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta;
+            do
+            {
+                Console.WriteLine("¿Quieres salir del programa? contesta SI si quieres salir");
+                respuesta = Console.ReadLine();
+
+            }
+            while (respuesta != "SI");
+            Console.WriteLine("Programa terminado");
+            Console.ReadKey();
+        }
+    }
+}
