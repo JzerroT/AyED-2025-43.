4ce4091448e383d2db31cee2e5e80db0e44181c9
JzerroT <janinatorrezmendoza@gmail.com>
2025-05-28 18:52:58 -0300

    Add files via upload

---
+++ Etapa 0/0-Torrez_4/0-Torrez_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Torrez_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("introdusca un numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("introdusca otro numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El total de la suma de esos dos numeros es: " + (num1 + num2));
+            Console.ReadKey();
+        }
+    }
+}
