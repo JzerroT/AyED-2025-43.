2bf3cfce005bc4b74082d8889208ecd7b1139d0e
JzerroT <janinatorrezmendoza@gmail.com>
2025-05-28 18:47:36 -0300

    Add files via upload

---
+++ 0-Torrez_1/0-Torrez_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Torrez_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hm = "Hola mundo";
+            Console.WriteLine(hm);
+            Console.ReadKey();
+        }
+    }
+}
