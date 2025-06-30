d0023bba57babade37dc23e16fe1dab725d9b42d
JzerroT <janinatorrezmendoza@gmail.com>
2025-06-30 03:40:28 -0300

    Add files via upload

---
+++ Etapa 0/0-Torrez_10/0-Torrez_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Torrez_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grados_c;
+            Console.WriteLine("introduci los grados celsius: ");
+            grados_c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(+grados_c + "grados celsius a kelvin: " + (grados_c + 273.15));
+            Console.WriteLine(+grados_c + "grados celsius a fahrenheit: " + ((grados_c * 9 / 5) + 32));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 0/0-Torrez_12/0-Torrez_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Torrez_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra1;
+            string letra2;
+            string letra3;
+            Console.WriteLine("introduci una letra: ");
+            letra1 = Console.ReadLine();
+            Console.WriteLine("introduci otra letra: ");
+            letra2 = Console.ReadLine();
+            Console.WriteLine("introduci otra letra mas: ");
+            letra3 = Console.ReadLine();
+            Console.WriteLine(letra3 + letra2 + letra1);
+            Console.ReadKey();
+        }
+    }
+}
