94d316135baa47b6a37f0285dba76640e0a470c6
JzerroT <janinatorrezmendoza@gmail.com>
2025-06-30 04:17:13 -0300

    Add files via upload

---
+++ Etapa 2/2-Torrez_10/2-Torrez_10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Torrez_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el tamaño de las matrices:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] matriz1 = new int[n, n];
+            int[,] matriz2= new int[n, n];
+            int[,] sumamatriz= new int[n,n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int J= 0; J < n; J++)
+                {
+                    matriz1[i, J] = rand.Next(1, 100);
+                    matriz2[i, J] = rand.Next(1, 100);
+                    sumamatriz[i, J] = matriz1[i, J] + matriz2[i, J];
+                }
+            }
+            Console.WriteLine("Matriz 1:");
+            Console.WriteLine(Matriz1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/2-Torrez_9/2-Torrez_9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Torrez_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese numero de fila: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero de columna: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] matriz = new int[n, m];
+           
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = rand.Next(1, 100);
+                }
+            }
+            Console.Write("Matriz: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i,j]+"  ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
